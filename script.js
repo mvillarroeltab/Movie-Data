f8a3877aff79769019ab2de800997133a8e7ab2d
MarinaVillarroel <m.villarroeltab@gmail.com>
2022-11-03 21:42:02 +0000

    Added the javascript to push another item into the movie array

--- script.js
+++ script.js
@@ -41,30 +41,78 @@ for (title in movieData) {
   movieArray.push(data);
 }
 
-console.log(movieArray);
 
 function render(items) {
 
   var index = 0;
+  var divId = index + 1;
+  document.getElementById('movie').innerHTML = "";
   while (index < items.length) {
     let item = items[index];
 
-
-    document.getElementById(`${index + 1}`).innerHTML =
-    `<img class="image-m" src="./images/${item.title}.jpg" alt="Movie1" />`
+    document.getElementById('movie').innerHTML +=
+    "<div class='box1'>"
+    +`<img class="image-m" src="./images/${item.title}.jpg" alt="Movie1" />`
     + "<h1>" + item.title + "</h1>"
     + "<br>"
     + "<p>" + item.plot + "</p>"
     + "<p>Rating: " + item.rating + "</p>"
     + "<p>Runtime: " + item.runtime + "</p>"
     + "<p>Year: " + item.year + "</p>"
-    + "<p>Cast: " + item.cast.toString() + "</p>";
+    + "<p>Cast: " + item.cast.join(", ") + "</p>"
+
+
+    "</div>";
 
     index = index + 1;
+
+
+    // document.getElementById(`${index + 1}`).innerHTML =
+    // `<img class="image-m" src="./images/${item.title}.jpg" alt="Movie1" />`
+    // + "<h1>" + item.title + "</h1>"
+    // + "<br>"
+    // + "<p>" + item.plot + "</p>"
+    // + "<p>Rating: " + item.rating + "</p>"
+    // + "<p>Runtime: " + item.runtime + "</p>"
+    // + "<p>Year: " + item.year + "</p>"
+    // + "<p>Cast: " + item.cast.join(", ") + "</p>";
+    //
+    // index = index + 1;
 
   }
 };
 render(movieArray);
+
+// Function to push a new movie into the movieArray array
+
+function addMovie (){
+  var title = document.getElementById('newTitle').value;
+  var plot = document.getElementById('newPlot').value;
+  var rating = document.getElementById('newRating').value;
+  var runtime = document.getElementById('newRuntime').value;
+  var year = document.getElementById('newYear').value;
+  var cast = document.getElementById('newCast').value;
+  event.preventDefault();
+  console.log(`title: ${title}`);
+  console.log(`plot: ${plot}`);
+  const newMovie = {
+    title: title,
+    plot: plot,
+    rating: rating,
+    runtime: runtime,
+    year: year,
+    cast: [cast]
+  };
+
+  movieArray.push(newMovie);
+  render(movieArray);
+
+
+}
+
+let addM = document.getElementById('submit');
+addM.addEventListener ('click', addMovie);
+
 
 // Function to sort by rating
 
